6a50c7223fbc366d9ba1b16be516ba04e1fed0cb
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2024-07-05 13:43:38 +0100

    Add some files to get going.

---
+++ main_window.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gedcom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AppExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the main window and exit the program.
+            Close();
+        }
+
+        private void AppTest_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser_.NavigateToString("<h1>Hello World</h1>");
+        }
+    }
+}
