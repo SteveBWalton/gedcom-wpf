830db1a74087a507af0c7a8cd430e892ef181951
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2025-03-09 15:46:52 +0000

    Add an date field to the TagDate class.

--- gedcom-cs/tag_date.cs
+++ gedcom-cs/tag_date.cs
@@ -14,6 +14,9 @@ namespace gedcom
         /// <summary>The actual date tag.</summary>
         private readonly Tag _tag;
 
+        /// <summary>An approximate date time object for sorting etc.</summary>
+        private readonly DateTime _dateTime;
+
         #endregion
 
         #region Class Constructors
@@ -22,7 +25,140 @@ namespace gedcom
         /// <param name="tag">Specifies the Tag object which is really expected to be of DATE type.</param>
         public TagDate(Tag tag)
         {
+            // Record the actual tag.
             _tag = tag;
+
+            // Try to get the approximate month.
+            int month;
+            int monthPos;
+            getMonth(out month, out monthPos);
+            if (monthPos == -1)
+            {
+                // No month information.
+                string yearString = getDigits(0, 1);
+                int year;
+                if (!int.TryParse(yearString, out year))
+                {
+                    year = 1990;
+                }
+                _dateTime = new DateTime(year, 1, 1);
+            }
+            else
+            {
+                // Search forward for year information and backwards for day information.
+                string yearString = getDigits(monthPos, 1);
+                string dayString = getDigits(monthPos, -1);
+                Console.WriteLine("'" + _tag.value + "' => '" + dayString + "', " + month.ToString() + ", '" + yearString + "'");
+                int year;
+                if (!int.TryParse(yearString,out year))
+                {
+                    year = 1990;
+                }
+                int day;
+                if(!int.TryParse(dayString,out day))
+                {
+                    day = 1;
+                }
+                _dateTime = new DateTime(year, month, day);
+            }
+            Console.WriteLine("'" + _tag.value + "' => " + _dateTime.ToString());
+        }
+
+        #endregion
+
+        #region Generate DateTime object.
+
+        /// <summary>Returns a value to use for the month and the position of this month information in the tag.</summary>
+        /// <param name="month">Returns the value to use for the month.</param>
+        /// <param name="monthPos">Returns the position in the tag where this month value from taken from.</param>
+        private void getMonth(out int month, out int monthPos)
+        {
+            const int MISSING = 9999;
+
+            // Default to no month information.
+            month = 1;
+            monthPos = -1;
+
+            // Search for a standard month name.
+            int firstMonthPos = MISSING;
+            int firstMonthValue = MISSING;
+            int pos;
+            month = 1;
+            foreach (string monthName in new string[] { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" })
+            {
+                if ((pos = _tag.value.ToUpper().IndexOf(monthName)) > 0)
+                {
+                    if (pos < firstMonthPos)
+                    {
+                        firstMonthPos = pos;
+                        firstMonthValue = month;
+                    }                    
+                }
+                month++;
+            }
+
+            // Search for other month type information.
+            // Might find an earlier month marker in the string.
+
+            // Return the first piece of month information.
+            if (firstMonthPos != MISSING)
+            {
+                month = firstMonthValue;
+                monthPos = firstMonthPos;
+            }
+        }
+
+
+        /// <summary>Search fowards or backwards in the tag value string for digits.</summary>
+        /// <param name="position">Specifies the position to search from.</param>
+        /// <param name="direction">Specifies the search direction.</param>
+        /// <returns></returns>
+        private string getDigits(int position, int direction)
+        {
+            // Find the starting position.
+            int startPosition = position;
+            while (!Char.IsDigit(_tag.value[startPosition]))
+            {
+                startPosition += direction;
+                if (startPosition < 0)
+                {
+                    startPosition = 0;
+                    break;
+                }
+                if (startPosition >= _tag.value.Length)
+                {
+                    startPosition = _tag.value.Length - 1;
+                    break;
+                }
+            }
+
+            // Find the ending position.
+            int endPosition = startPosition;
+            while (Char.IsDigit(_tag.value[endPosition]))
+            {
+                endPosition += direction;
+                if (endPosition < 0)
+                {
+                    endPosition = 0;
+                    break;
+                }
+                if (endPosition >= _tag.value.Length)
+                {
+                    endPosition = _tag.value.Length - 1;
+                    break;
+                }
+            }
+
+            // Switch start and end into assending order.
+            if (startPosition > endPosition)
+            {
+                int swap = startPosition;
+                startPosition = endPosition;
+                endPosition = swap;
+            }
+
+            // Return the sub string identified.
+            return _tag.value.Substring(startPosition, endPosition - startPosition + 1);
         }
 
         #endregion
@@ -32,7 +168,7 @@ namespace gedcom
         public string getLongDate()
         {
             // Check for an empty / missing tag.
-            if (_tag==null)
+            if (_tag == null)
             {
                 return "";
             }
