be98fe52d1fa601803e16bb0a4d75014d9447479
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2025-04-14 07:50:11 +0100

    Split the TreeFamilyLine class into its own file.

--- Viewer/render_tree.cs
+++ Viewer/render_tree.cs
@@ -65,7 +65,7 @@ namespace gedcom.viewer
 
         /// <summary>The height of lines to the familes on each row.</summary>
         /// <remarks>Do something better than an encoded string!</remarks>
-        private List<FamilyLine>[] _familyLines;
+        private List<TreeFamilyLine>[] _familyLines;
 
         /// <summary>The number of lower lines that are taken.</summary>
         private int [] _lowerLines;
@@ -238,8 +238,8 @@ namespace gedcom.viewer
                 {
                     for (int family2Index = family1Index + 1; family2Index < _familyLines[row].Count; family2Index++)
                     {
-                        FamilyLine family1 = _familyLines[row][family1Index];
-                        FamilyLine family2 = _familyLines[row][family2Index];
+                        TreeFamilyLine family1 = _familyLines[row][family1Index];
+                        TreeFamilyLine family2 = _familyLines[row][family2Index];
                         if (row >= DEBUG_ROW)
                         {
                             Console.WriteLine("family " + family1Index.ToString() + " left = " + family1.leftPos.ToString() + ", right = " + family1.rightPos.ToString() + ", parent = " + family1.parentJoinPosition.ToString() + ", lineHeight = " + family1.lineHeight.ToString());
@@ -285,7 +285,7 @@ namespace gedcom.viewer
             y = INDIVIDUAL_HEIGHT;
             for (int row = 0; row < 5; row++)
             {
-                foreach(FamilyLine familyTree in _familyLines[row])
+                foreach(TreeFamilyLine familyTree in _familyLines[row])
                 {
                     // Draw line down from family / person.
                     int connectionY = y + BAR_POSITION + familyTree.lineHeight;
@@ -511,7 +511,7 @@ namespace gedcom.viewer
         /// <returns></returns>
         private int getFamilyBarHeight(int level, int indvidualColumn, int familyColumn)
         {
-            foreach (FamilyLine familyLine in _familyLines[level])
+            foreach (TreeFamilyLine familyLine in _familyLines[level])
             {
                 //string[] data = line.Split(';');
                 //int dataColumn = int.Parse(data[0]);
@@ -527,7 +527,7 @@ namespace gedcom.viewer
             }
 
             // Save for next time.
-            FamilyLine newFamilyLine = new FamilyLine(familyColumn);
+            TreeFamilyLine newFamilyLine = new TreeFamilyLine(familyColumn);
             newFamilyLine.lineHeight = (VERTICAL_SPACE - BAR_POSITION) / 2;
             newFamilyLine.children.Add(indvidualColumn);
             _familyLines[level].Add(newFamilyLine);
@@ -546,13 +546,13 @@ namespace gedcom.viewer
         {
             // Build a grid of individuals to show.
             _grid = new List<string>[5];
-            _familyLines = new List<FamilyLine>[5];
+            _familyLines = new List<TreeFamilyLine>[5];
             _lowerLines = new int[5];
             _higherLines = new int[5];
             for (int i = 0; i < 5; i++)
             {
                 _grid[i] = new List<string>();
-                _familyLines[i] = new List<FamilyLine>();
+                _familyLines[i] = new List<TreeFamilyLine>();
                 _lowerLines[i] = 0;
                 _higherLines[i] = 0;
             }
@@ -818,104 +818,5 @@ namespace gedcom.viewer
         #endregion
 
 
-    }
-
-
-    /// <summary>
-    /// Class to represent a line from an individual to their parent family.
-    /// </summary>
-    public class FamilyLine
-    {
-        #region Member Variables
-
-        /// <summary>The position of the family / parent that this line connects to.</summary>
-        private int _parentJoinPosition;
-
-        /// <summary>The height of the connection bar for this family.</summary>
-        private int _lineHeight;
-
-        /// <summary>The positions of the children that connect to this family.</summary>
-        private readonly List<int> _children;
-
-        #endregion
-
-        #region Class Constructors
-
-        public FamilyLine(int parentJoinPosition)
-        {
-            _parentJoinPosition = parentJoinPosition;
-            _lineHeight = 0;
-            _children = new List<int>();
-        }
-
-        #endregion
-
-        #region Properties.
-
-        /// <summary>The position of the family / parent that this line connects to.</summary>
-        public int parentJoinPosition
-        {
-            get { return _parentJoinPosition; }
-        }
-
-        /// <summary>The height of the connection bar for this family.</summary>
-        public int lineHeight
-        {
-            get { return _lineHeight; }
-            set { _lineHeight = value; }
-        }
-
-        /// <summary>
-        /// The positions of the children that connect to this family.
-        /// </summary>
-        public List<int> children
-        {
-            get { return _children; }
-        }
-
-        /// <summary>The left most position.</summary>
-        public int leftPos
-        {
-            get
-            {
-                if (_children.Count == 0)
-                {
-                    return _parentJoinPosition;
-                }
-                int leftPos = _parentJoinPosition;
-                foreach (int childPos in _children)
-                {
-                    if (childPos < leftPos)
-                    {
-                        leftPos = childPos;
-                    }
-                }
-                return leftPos;
-            }
-        }
-
-        /// <summary>The right most position.</summary>
-        public int rightPos
-        {
-            get
-            {
-                if (_children.Count == 0)
-                {
-                    return _parentJoinPosition;
-                }
-                int rightPos = _parentJoinPosition;
-                foreach (int childPos in _children)
-                {
-                    if (childPos > rightPos)
-                    {
-                        rightPos = childPos;
-                    }
-                }
-                return rightPos;
-            }
-        }
-
-
-        #endregion
     }
 }
---
+++ Viewer/tree_family_line.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gedcom.viewer
+{
+    /// <summary>Class to represent a line from an individual to their parent family on a tree.</summary>
+    public class TreeFamilyLine
+    {
+        #region Member Variables
+
+        /// <summary>The position of the family / parent that this line connects to.</summary>
+        private int _parentJoinPosition;
+
+        /// <summary>The height of the connection bar for this family.</summary>
+        private int _lineHeight;
+
+        /// <summary>The positions of the children that connect to this family.</summary>
+        private readonly List<int> _children;
+
+        #endregion
+
+        #region Class Constructors
+
+        public TreeFamilyLine(int parentJoinPosition)
+        {
+            _parentJoinPosition = parentJoinPosition;
+            _lineHeight = 0;
+            _children = new List<int>();
+        }
+
+        #endregion
+
+        #region Properties.
+
+        /// <summary>The position of the family / parent that this line connects to.</summary>
+        public int parentJoinPosition
+        {
+            get { return _parentJoinPosition; }
+        }
+
+
+
+        /// <summary>The height of the connection bar for this family.</summary>
+        public int lineHeight
+        {
+            get { return _lineHeight; }
+            set { _lineHeight = value; }
+        }
+
+
+
+        /// <summary>The positions of the children that connect to this family.</summary>
+        public List<int> children
+        {
+            get { return _children; }
+        }
+
+
+
+        /// <summary>The left most position in this family.</summary>
+        public int leftPos
+        {
+            get
+            {
+                if (_children.Count == 0)
+                {
+                    return _parentJoinPosition;
+                }
+                int leftPos = _parentJoinPosition;
+                foreach (int childPos in _children)
+                {
+                    if (childPos < leftPos)
+                    {
+                        leftPos = childPos;
+                    }
+                }
+                return leftPos;
+            }
+        }
+
+
+
+        /// <summary>The right most position in this family.</summary>
+        public int rightPos
+        {
+            get
+            {
+                if (_children.Count == 0)
+                {
+                    return _parentJoinPosition;
+                }
+                int rightPos = _parentJoinPosition;
+                foreach (int childPos in _children)
+                {
+                    if (childPos > rightPos)
+                    {
+                        rightPos = childPos;
+                    }
+                }
+                return rightPos;
+            }
+        }
+
+        #endregion
+    }
+}
