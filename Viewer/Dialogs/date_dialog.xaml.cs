47f2dec3bf079d575598faa1a53c0f06fdd954b1
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2025-06-23 14:43:06 +0100

    The date helper dialog is working.

--- Viewer/Dialogs/date_dialog.xaml.cs
+++ Viewer/Dialogs/date_dialog.xaml.cs
@@ -44,6 +44,8 @@ namespace gedcom.viewer
         {
             #region Member Variables
 
+            static string[] monthNames = new string[] { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+
             /// <summary>The on before after status of this date.</summary>
             public OnBeforeAfter onBeforeAfter;
 
@@ -230,6 +232,8 @@ namespace gedcom.viewer
                 return workingString.Substring(startPosition, endPosition - startPosition + 1);
             }
 
+
+
             /// <summary>Convert the class member variables into a string.</summary>
             /// <returns>The string that represents the class member variables.</returns>
             public override string ToString()
@@ -248,6 +252,22 @@ namespace gedcom.viewer
                 if (isAbout)
                 {
                     result.Append("ABT ");
+                }
+
+                if (!isDayUnknown)
+                {
+                    result.Append(day.ToString());
+                    result.Append(" ");
+                }
+                if (!isMonthUnknown)
+                {
+                    result.Append(monthNames[month - 1]);
+                    result.Append(" ");
+                }
+                if (!isYearUnknown)
+                {
+                    result.Append(year.ToString());
+                    result.Append(" ");
                 }
 
                 return result.ToString();
@@ -429,7 +449,7 @@ namespace gedcom.viewer
 
             txtDay.Text = dialogDate.day.ToString();
             txtYear.Text = dialogDate.year.ToString();
-            cboMonth.SelectedIndex = dialogDate.month;
+            cboMonth.SelectedIndex = dialogDate.month - 1;
 
             chkDayUnknown.IsChecked = dialogDate.isDayUnknown;
             chkMonthUnknown.IsChecked = dialogDate.isMonthUnknown;
@@ -438,6 +458,41 @@ namespace gedcom.viewer
             txtDay.Visibility = dialogDate.isDayUnknown ? Visibility.Hidden : Visibility.Visible;
             cboMonth.Visibility = dialogDate.isMonthUnknown ? Visibility.Hidden : Visibility.Visible;
             txtYear.Visibility = dialogDate.isYearUnknown ? Visibility.Hidden : Visibility.Visible;
+
+            // Return success.
+            return true;
+        }
+
+
+
+        /// <summary>Transfer the values from the member variables to the dialog text box.</summary>
+        /// <returns>True for success, false otherwise.</returns>
+        private bool fromDialog()
+        {
+            // Build a new date string.
+            StringBuilder newDate = new StringBuilder();
+
+            if (_onFromBetween == OnFromBetween.ON)
+            {
+                newDate.Append(_firstDate.ToString());
+            }
+            else if (_onFromBetween == OnFromBetween.FROM)
+            {
+                newDate.Append("FROM ");
+                newDate.Append(_firstDate.ToString());
+                newDate.Append("TO ");
+                newDate.Append(_secondDate.ToString());
+            }
+            else if (_onFromBetween == OnFromBetween.BETWEEN)
+            {
+                newDate.Append("BET ");
+                newDate.Append(_firstDate.ToString());
+                newDate.Append("AND ");
+                newDate.Append(_secondDate.ToString());
+            }
+
+            // Update the text control.
+            _txtTagDate.Text = newDate.ToString();
 
             // Return success.
             return true;
@@ -492,6 +547,9 @@ namespace gedcom.viewer
             // Hide the second date.
             _secondDateOnBeforeAfter.Visibility = Visibility.Hidden;
             _secondDateGrid.Visibility = Visibility.Hidden;
+
+            // Update the text value.
+            fromDialog();
         }
 
 
@@ -510,6 +568,9 @@ namespace gedcom.viewer
             // Show the second date.
             _secondDateOnBeforeAfter.Visibility = Visibility.Visible;
             _secondDateGrid.Visibility = Visibility.Visible;
+
+            // Update the text value.
+            fromDialog();
         }
 
 
@@ -528,6 +589,9 @@ namespace gedcom.viewer
             // Show the second date.
             _secondDateOnBeforeAfter.Visibility = Visibility.Visible;
             _secondDateGrid.Visibility = Visibility.Visible;
+
+            // Update the text value.
+            fromDialog();
         }
 
 
@@ -543,7 +607,350 @@ namespace gedcom.viewer
             toDialog();
         }
 
+
+
+        /// <summary>Signal handler for the first date On checked.</summary>
+        private void radiobuttonFirstOnChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.onBeforeAfter = OnBeforeAfter.ON;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date Before checked.</summary>
+        private void radiobuttonFirstBeforeChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.onBeforeAfter = OnBeforeAfter.BEFORE;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date After checked.</summary>
+        private void radiobuttonFirstAfterChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.onBeforeAfter = OnBeforeAfter.AFTER;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date On checked.</summary>
+        private void radiobuttonSecondOnChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.onBeforeAfter = OnBeforeAfter.ON;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date Before checked.</summary>
+        private void radiobuttonSecondBeforeChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.onBeforeAfter = OnBeforeAfter.BEFORE;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date After checked.</summary>
+        private void radiobuttonSecondAfterChecked(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.onBeforeAfter = OnBeforeAfter.AFTER;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date about changed.</summary>
+        private void checkboxFirstAboutChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.isAbout = _checkboxFirstAbout.IsChecked == true;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date about changed.</summary>
+        private void checkboxSecondAboutChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.isAbout = _checkboxSecondAbout.IsChecked == true;
+            // Update the text value.
+            fromDialog();
+        }
+
         #endregion
 
+        /// <summary>Signal handler for the first date day unknown changed.</summary>
+        private void chkFirstDayUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.isDayUnknown = _chkFirstDayUnknown.IsChecked == true;
+            _txtFirstDay.Visibility = _firstDate.isDayUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date day unknown changed.</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chkSecondDayUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.isDayUnknown = _chkSecondDayUnknown.IsChecked == true;
+            _txtSecondDay.Visibility = _secondDate.isDayUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date day value losing focus.</summary>
+        private void txtFirstDayLostFocus(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            int day = 0;
+            if (int.TryParse(_txtFirstDay.Text, out day))
+            {
+                _firstDate.day = day;
+                // Update the text value.
+                fromDialog();
+            }
+        }
+
+
+
+        /// <summary>Signal handler for the second date day value losing focus.</summary>
+        private void txtSecondDayLostFocus(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            int day = 0;
+            if (int.TryParse(_txtSecondDay.Text, out day))
+            {
+                _secondDate.day = day;
+                // Update the text value.
+                fromDialog();
+            }
+        }
+
+
+
+        /// <summary>Signal handler for the first date month unknown changing.</summary>
+        private void chkFirstMonthUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.isMonthUnknown = _chkFirstMonthUnknown.IsChecked == true;
+            _cboFirstMonth.Visibility = _firstDate.isMonthUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date month unknown changing.</summary>
+        private void chkSecondMonthUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.isMonthUnknown = _chkSecondMonthUnknown.IsChecked == true;
+            _cboSecondMonth.Visibility = _secondDate.isMonthUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first month changing value.</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cboFirstMonthSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.month = 1 + _cboFirstMonth.SelectedIndex;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second month changing value.</summary>
+        private void cboSecondMonthSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.month = 1 + _cboSecondMonth.SelectedIndex;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date year unknown changing.</summary>
+        private void chkFirstYearUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _firstDate.isYearUnknown = _chkFirstYearUnknown.IsChecked == true;
+            _txtFirstYear.Visibility = _firstDate.isYearUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the second date year unknown changing.</summary>
+        private void chkSecondYearUnknownChanged(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            _secondDate.isYearUnknown = _chkSecondYearUnknown.IsChecked == true;
+            _txtSecondYear.Visibility = _secondDate.isYearUnknown ? Visibility.Hidden : Visibility.Visible;
+            // Update the text value.
+            fromDialog();
+        }
+
+
+
+        /// <summary>Signal handler for the first date year textbox losing the focus.</summary>
+        private void txtFirstYearLostFocus(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            int year = 0;
+            if (int.TryParse(_txtFirstYear.Text, out year))
+            {
+                _firstDate.year = year;
+                // Update the text value.
+                fromDialog();
+            }
+        }
+
+
+
+        /// <summary>Signal handler for the second date year textbox losing the focus.</summary>
+        private void txtSecondYearLostFocus(object sender, RoutedEventArgs e)
+        {
+            // Check for updates allowed.
+            if (_isNoUpdate)
+            {
+                return;
+            }
+
+            int year = 0;
+            if (int.TryParse(_txtSecondYear.Text, out year))
+            {
+                _secondDate.year = year;
+                // Update the text value.
+                fromDialog();
+            }
+        }
     }
 }
