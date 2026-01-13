b1efdffc2ee73a79dfb13bbfda07f74685fd12f6
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2026-01-13 08:25:50 +0000

    Define a layout for the edit source dialog.

--- Viewer/Dialogs/source.xaml.cs
+++ Viewer/Dialogs/source.xaml.cs
@@ -17,26 +17,41 @@ namespace gedcom.viewer
     /// <summary>Class to represent the edit source dialog.</summary>
     public partial class DialogSource : Window
     {
+        #region Constructors
+
         /// <summary>Default constructor for the edit source dialog.</summary>
         public DialogSource()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Signal Handlers
+
+        /// <summary>Signal handler for the OK button click event.</summary>
         private void buttonOkClick(object sender, RoutedEventArgs e)
         {
             // Close the dialog with okay.
             this.DialogResult = true;
         }
 
+
+
+        /// <summary>Signal handler for the cancel button click.</summary>
         private void buttonCancelClick(object sender, RoutedEventArgs e)
         {
 
         }
 
+
+
+        /// <summary>Signal handler for the add tag button click.</summary>
         private void addTagButtonClick(object sender, RoutedEventArgs e)
         {
 
         }
+
+        #endregion
     }
 }
